de92f095a67422f9632631ca3721e6875d2d708f
Warren Kenny <wkenny@riotgames.com>
2022-12-06 09:20:49 +0000

    Added solution for Day 6.

---
+++ day6/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::{VecDeque, HashSet};
+
+fn find_sequence_position(datastream: &String, length: usize) -> Option<usize> {
+    let mut buffer: VecDeque<char> = VecDeque::new();
+    for (i, c) in datastream.char_indices() {
+        if buffer.len() == length {
+            buffer.pop_back();
+        }
+
+        buffer.push_front(c);
+        let set: HashSet<char> = HashSet::from_iter(buffer.iter().cloned());
+
+        if set.len() == length {
+            return Some(i + 1);
+        }
+    }
+
+    return None;
+}
+
+fn main() {
+    let mut datastream = String::new();
+
+    fn reducer(line: &str, datastream: &mut String) {
+        datastream.push_str(line);
+    }
+
+    if !aoc::reduce_input(6, &mut datastream, reducer) {
+        panic!("Failed to read input");
+    }
+
+    println!("Input Length: {}", datastream.len());
+
+    println!("Start-of-Packet after character {}", find_sequence_position(&datastream, 4).expect("Failed to find start-of-packet position"));
+    println!("Start-of-Message after character {}", find_sequence_position(&datastream, 14).expect("Failed to find start-of-message position"));
+}
